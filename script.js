e0238db0736d1fc04c2928259d8f4cbef662e9b8
mr-hare-cca <150952432+mr-hare-cca@users.noreply.github.com>
2025-10-21 16:32:12 +0000

    test 1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function calculate() {
+  let tempF = parseFloat(document.getElementById("temp").value);
+  let radius = parseFloat(document.getElementById("radius").value);
 
+  let celsius = (tempF - 32) * 5 / 9;
+  let area = Math.PI * Math.pow(radius, 2);
+
+  document.getElementById("tempC").textContent = celsius.toFixed(2);
+  document.getElementById("area").textContent = area.toFixed(2);
+}
